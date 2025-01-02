1a9af68b4ba9974cebeee99ea1caff40b2f3eee8
Aathira <23041002@myrp.edu.sg>
2025-01-02 16:06:06 +0800

    hi

--- src/App.js
+++ src/App.js
@@ -5,15 +5,31 @@ function App() {
   const [movies, setMovies] = useState([]);
   const [query, setQuery] = useState("batman");
  
+  // useEffect(() => {
+  //   const controller = new AbortController();
+  //   fetch(`https://www.omdbapi.com/?apikey=${KEY}&s=${query}`, { signal: controller.signal })
+  //       .then((res) => res.json())
+  //       .then((data) => data.Response === "True" && setMovies(data.Search))
+  //       .catch((err) => console.log(err));
+  //   return () => controller.abort();
+  // }, [query]);
+
   useEffect(() => {
     const controller = new AbortController();
-    fetch(`https://www.omdbapi.com/?apikey=${KEY}&s=${query}`, { signal: controller.signal })
-        .then((res) => res.json())
-        .then((data) => data.Response === "True" && setMovies(data.Search))
-        .catch((err) => console.log(err));
+    const fetchMovies = async () => {
+      try {
+        const res = await fetch(`https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+         { signal: controller.signal });
+        const data = await res.json();
+        data.Response === "True" && setMovies(data.Search);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchMovies();
     return () => controller.abort();
   }, [query]);
- 
+
   return (
     <div>
       <h1>Movies</h1>
